97f7964547d21586c2578aceef539cde17505b66
Mericantik <146951316+Mericantik@users.noreply.github.com>
2024-01-15 14:59:47 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 REPO="https://github.com/naya1503/Naya-Userbot.git"
-DIR="/root/naya1503"
+DIR="/root/Mericantik"
 
 spinner(){
     local pid=$!
@@ -31,7 +31,7 @@ clone_repo(){
             fi
             return
     fi
-    echo -e "Cloning Naya-Userbot ${BRANCH}... "
+    echo -e "Cloning Zennnith-Userbot ${BRANCH}... "
     git clone -b $BRANCH $REPO $DIR
 }
 
